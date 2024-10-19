aa232be6d540e0efe826e92e40562d8a549a3724
kasjke <kasmatuy2k17@gmail.com>
2024-10-20 01:02:59 +0300

    first commit

---
+++ src/main/java/com/example/urbanmarket/UrbanMarketApplication.java
@@ -0,0 +1,13 @@
+package com.example.urbanmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrbanMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/urbanmarket/UrbanMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.urbanmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbanMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
